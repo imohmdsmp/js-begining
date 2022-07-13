23422da8b47953f120012d4693e7572e431d44e9
Mohammad <imohmdsmp@gmail.com>
2022-07-13 11:06:45 +0430

    new

---
+++ js/master.js
@@ -0,0 +1,20 @@
+alert("Welcome");
+let fname = prompt("Please enter your name");
+let lname = prompt("please enter your last name");
+let age = prompt("please enter your age");
+age = Number(age);
+console.log(typeof age)
+document.write("your name is " + fname + lname + " you are " + age +"`s"+ "old" + "<br>" );
+
+
+let num1 = "2";
+let num2 = "8";
+
+document.write("number is : " + num1 + num2 + "<br>");
+document.write("number is : " + num1 - num2 + "<br>");
+document.write("number is : " + num1 / num2 + "<br>");
+document.write("number is : " + num1 * num2 + "<br>");
+document.write("number is :" + num1 ** num2 + "<br>");
+
+
+
